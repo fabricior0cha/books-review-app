cbad48be64108c47520a6ea904b019c92f6a0e8a
Fabricio <fabriciorochapalmeira@gmail.com>
2024-05-11 18:54:30 -0300

    chore: config next base path for public

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/books-review-app",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
